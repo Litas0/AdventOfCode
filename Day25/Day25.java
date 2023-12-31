0b468a4b2c27926aac70af8b6273a9d137cf9135
Litas0 <kubalitke123@gmail.com>
2023-12-31 11:57:30 +0100

    Adding few last days of Advent Of Code

---
+++ Day25/Day25.java
@@ -0,0 +1,147 @@
+package Day25;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Day25 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("E:\\Codes\\AdventOfCode\\Day25\\input25.txt"));
+        HashMap<String, Vertex> vertices = new HashMap<>();
+        List<Edge> edges = new ArrayList<>();
+        String s;
+        while ((s = br.readLine()) != null) {
+            String[] parts = s.split(": ");
+            Vertex nv = vertices.computeIfAbsent(parts[0], Vertex::new);
+            String[] neighbours = parts[1].split(" ");
+            for (String nb : neighbours) {
+                Vertex nv2 = vertices.computeIfAbsent(nb, Vertex::new);
+                Edge edge = new Edge(nv, nv2);
+                edges.add(edge);
+                nv.addNeighbour(edge);
+                nv2.addNeighbour(edge);
+            }
+        }
+
+        int tries = 3000;
+        List<Vertex> vertexList = new ArrayList<>(vertices.values());
+        Random r = new Random();
+        for (int i = 0; i < tries; i++) {
+            if (i % 1000 == 0) {
+                System.out.println(i);
+            }
+            Vertex start = vertexList.get(r.nextInt(vertices.size()));
+            Vertex end = vertexList.get(r.nextInt(vertices.size()));
+            dijkstra(start, end);
+        }
+
+        edges.sort((o1, o2) -> o2.counter - o1.counter);
+        for (Edge e : edges) {
+            System.out.println(e);
+        }
+
+        Edge disconnect = edges.getFirst();
+        Vertex start1 = disconnect.one;
+        Vertex start2 = disconnect.two;
+
+        HashSet<Edge> disconnected = new HashSet<>();
+        disconnected.add(edges.get(0));
+        disconnected.add(edges.get(1));
+        disconnected.add(edges.get(2));
+        int size1 = flood(start1, vertices, disconnected);
+        int size2 = flood(start2, vertices, disconnected);
+        System.out.printf("%d = %d + %d\n", vertexList.size(), size1, size2);
+        System.out.println(size1 * size2);
+        br.close();
+    }
+
+    private static int flood(Vertex start, HashMap<String, Vertex> vertices, HashSet<Edge> disconnected) {
+        HashSet<Vertex> seen = new HashSet<>();
+        LinkedList<Vertex> todo = new LinkedList<>();
+        todo.add(start);
+        while (!todo.isEmpty()) {
+            Vertex cur = todo.removeFirst();
+            if (seen.contains(cur)) {
+                continue;
+            }
+            seen.add(cur);
+            for (Edge e : cur.outgoing) {
+                if (disconnected.contains(e)) {
+                    continue;
+                }
+                todo.add(e.other(cur));
+            }
+        }
+        return seen.size();
+    }
+
+    record WorkItem(Vertex v, int steps, Edge used, WorkItem prev){
+        @Override
+        public String toString() {
+            return v.label + " " + steps;
+        }
+    }
+
+    private static void dijkstra(Vertex start, Vertex end) {
+        PriorityQueue<WorkItem> todo = new PriorityQueue<>(Comparator.comparingInt(o -> o.steps));
+        HashMap<Vertex, Integer> shortest = new HashMap<>();
+        todo.add(new WorkItem(start, 0, null, null));
+        while (!todo.isEmpty()) {
+            WorkItem wi = todo.remove();
+            int way = shortest.computeIfAbsent(wi.v, v -> Integer.MAX_VALUE);
+            if (way <= wi.steps) {
+                continue;
+            }
+            if (end == wi.v) {
+                WorkItem cur = wi;
+                while (cur.used != null) {
+                    cur.used.counter++;
+                    cur = cur.prev;
+                }
+                return;
+            }
+            shortest.put(wi.v, wi.steps);
+            for (Edge e : wi.v.outgoing) {
+                Vertex cand = e.other(wi.v);
+                int cur = shortest.computeIfAbsent(cand, v -> Integer.MAX_VALUE);
+                if (cur > wi.steps + 1) {
+                    todo.add(new WorkItem(cand, wi.steps +1, e, wi));
+                }
+            }
+        }
+    }
+
+    static class Vertex {
+        List<Edge> outgoing = new ArrayList<>();
+        String label;
+
+        public Vertex(String label) {
+            this.label = label;
+        }
+
+        public void addNeighbour(Edge edge) {
+            outgoing.add(edge);
+        }
+    }
+
+    static class Edge {
+        Vertex one, two;
+        int counter;
+
+        public Edge(Vertex one, Vertex two) {
+            this.one = one;
+            this.two = two;
+        }
+
+        public Vertex other(Vertex v) {
+            return v == one ? two : one;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s -> %s : %d", one.label, two.label, counter);
+        }
+    }
+}
